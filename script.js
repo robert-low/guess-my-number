c2604387ad05a6a00c343028e2c7f643840ee4a9
Robert Low <robert.low91@gmail.com>
2023-04-13 13:34:58 +0100

    query selectors

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct number 🎉';
+console.log(
+  (document.querySelector('.message').textContent = 'Correct number 🎉')
+);
+
+document.querySelector('.number').textContent = 14;
+document.querySelector('.guess').value = 600;
+console.log(document.querySelector('.guess').value);
+document.querySelector('.score').textContent = 13;
