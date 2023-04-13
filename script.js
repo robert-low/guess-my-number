d9616611817864bd75732c030e1e07b57feebb79
Robert Low <robert.low91@gmail.com>
2023-04-13 14:03:05 +0100

    no number logic

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct number 🎉';
-console.log(
-  (document.querySelector('.message').textContent = 'Correct number 🎉')
-);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct number 🎉';
+// console.log(
+//   (document.querySelector('.message').textContent = 'Correct number 🎉')
+// );
 
-document.querySelector('.number').textContent = 14;
-document.querySelector('.guess').value = 600;
-console.log(document.querySelector('.guess').value);
-document.querySelector('.score').textContent = 13;
+// document.querySelector('.number').textContent = 14;
+
+// document.querySelector('.guess').value = 600;
+// console.log(document.querySelector('.guess').value);
+
+// document.querySelector('.score').textContent = 13;
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      '✋ You must enter a number.';
+  }
+});
